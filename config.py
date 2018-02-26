76defbc35df568f65aa414619228b0a4ac0d0e20
Postmodernist <aleksiarts@gmail.com>
2018-02-26 23:33:02 +0300

    Mcts fix

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Self play
-EPISODES = 150  # number of self-play games (AZ: 25000)
+EPISODES = 100  # number of self-play games (AZ: 25000)
 TAU = 2  # temperature, parameter controlling exploration
 
 # Evaluation
@@ -12,20 +12,21 @@ C = 1.4  # coefficient of U
 
 # Model retraining
 MEMORY_SIZE = 100000  # number of evaluated positions from last games (AZ: ~100,000,000)
-BATCH_SIZE = 1024  # mini-batch size (AZ: 2048)
+BATCH_SIZE = 512  # mini-batch size (AZ: 2048)
 EPOCHS = 3
 REG_CONST = 0.0001
 LEARNING_RATE = 0.1
 MOMENTUM = 0.9
 TRAINING_LOOPS = 20
 HIDDEN_CNN_LAYERS = [  # (AZ: 40 layers, 256 filters, 3x3 kernel)
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)},
-    {'filters': 128, 'kernel_size': (4, 4)}]
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)}]
+
